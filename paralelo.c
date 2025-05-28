97a8a9be806fe9630f53104c4fddc2269d737c20
Elocinn <100329391+Elocinn@users.noreply.github.com>
2025-05-28 20:59:10 -0300

    Update paralelo.c

--- paralelo.c
+++ paralelo.c
@@ -1,62 +1,111 @@
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi.h>
 
-#define N 1000
+#define N 4  // tamanho da matriz (NxN)
+
+void imprimirMatriz(int mat[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main(int argc, char *argv[]) {
     int rank, size;
+    int A[N][N], B[N][N], C[N][N];
+    int local_A[N];
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // Matrizes A, B e C globais
-    double A[N][N], B[N][N], C[N][N];
-
-    // Matrizes locais
-    double A_local[N/size][N], C_local[N/size][N];
-
-    // Inicializar a matriz B (matriz global)
-    if (rank == 0) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                B[i][j] = (double)(j + 1);
-            }
-        }
+    if (size != N) {
+        if (rank == 0)
+            printf("Este programa requer exatamente %d processos.\n", N);
+        MPI_Finalize();
+        return 0;
     }
 
-    // Distribute the matrix A to all processes
-    MPI_Scatter(A, (N*N/size), MPI_DOUBLE, A_local, (N*N/size), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    // Inicialização das matrizes no processo 0
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-    // Broadcast matrix B to all processes (since B is used by all processes)
-    MPI_Bcast(B, N*N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+#define N 4  // tamanho da matriz (NxN)
 
-    // Start time for matrix multiplication
-    double t0 = MPI_Wtime();  
-
-    // Perform local matrix multiplication
-    int linhas_por_processo = N / size;
-    for (int i = 0; i < linhas_por_processo; i++) {
+void imprimirMatriz(int mat[N][N]) {
+    for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            C_local[i][j] = 0.0;
-            for (int k = 0; k < N; k++) {
-                C_local[i][j] += A_local[i][k] * B[k][j];
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int rank, size;
+    int A[N][N], B[N][N], C[N][N];
+    int local_A[N];
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (size != N) {
+        if (rank == 0)
+            printf("Este programa requer exatamente %d processos.\n", N);
+        MPI_Finalize();
+        return 0;
+    }
+
+    // Inicialização das matrizes no processo 0
+   if (rank == 0) {
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) {
+                A[i][j] = i + j + 1;
+                B[i][j] = (i == j) ? 1 : 0;
             }
+    }
+
+    // Broadcast da matriz B
+    MPI_Bcast(B, N*N, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Scatter de linhas da matriz A
+    MPI_Scatter(A, N, MPI_INT, local_A, N, MPI_INT, 0, MPI_COMM_WORLD);
+
+    // Tempo de início (depois do Scatter)
+    double start_time = MPI_Wtime();
+
+    // Cálculo de uma linha da matriz C
+    int local_C[N];
+    for (int j = 0; j < N; j++) {
+        local_C[j] = 0;
+        for (int k = 0; k < N; k++) {
+            local_C[j] += local_A[k] * B[k][j];
         }
     }
 
-    // End time for matrix multiplication
-    double t1 = MPI_Wtime();
+    double end_time = MPI_Wtime();
+    double local_duration = end_time - start_time;
 
-    // Collect the local matrices C_local to the global matrix C
-    MPI_Gather(C_local, (N*N/size), MPI_DOUBLE, C, (N*N/size), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    // Gather das linhas calculadas
+    MPI_Gather(local_C, N, MPI_INT, C, N, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Print the time taken for matrix multiplication
+    // Obter o tempo máximo entre os processos
+    double max_duration;
+    MPI_Reduce(&local_duration, &max_duration, 1, MPI_DOUBLE, MPI_MAX, 0, MPI>
+
+    // Apenas o processo 0 imprime o resultado
     if (rank == 0) {
-        printf("Tempo total de multiplicação: %f segundos\n", t1 - t0);
+        printf("Resultado da multiplicação (MPI):\n");
+        imprimirMatriz(C);
+        printf("Tempo de execução (paralelo, MPI): %f segundos\n", max_durati>
     }
 
-    // Finalize MPI environment
     MPI_Finalize();
     return 0;
 }
+
