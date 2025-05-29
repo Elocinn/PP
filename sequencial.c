939678437a8a3f73aec8893edbdfec14b6e4dde3
Elocinn <100329391+Elocinn@users.noreply.github.com>
2025-05-28 21:01:57 -0300

    Update sequencial.c

--- sequencial.c
+++ sequencial.c
@@ -1,39 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/time.h>
+#include <time.h>
 
-#define N 1000
+#define N 4  // tamanho da matriz (NxN)
 
-double A[N][N], B[N][N], R[N][N];
-
-int main() {
-    // Inicialização das matrizes A e B
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            A[i][j] = (double)(i + 1);
-            B[i][j] = (double)(j + 1);
-        }
-    }
-
-    // Medir o tempo de execução
-    struct timeval start, end;
-    gettimeofday(&start, NULL);
-
-    // Multiplicação de matrizes (algoritmo cúbico)
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            R[i][j] = 0.0;
-            for(int k = 0; k < N; k++){
-                R[i][j] += A[i][k] * B[k][j];
+void multiplicarMatrizes(int A[N][N], int B[N][N], int C[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
+}
 
-    gettimeofday(&end, NULL);
+void imprimirMatriz(int M[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", M[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-    // Calcular o tempo em segundos
-    double tempo = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) * 1e-6;
-    printf("Tempo sequencial: %f segundos\n", tempo);
+int main() {
+    int A[N][N], B[N][N], C[N][N];
+
+    // Inicialização das matrizes
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) {
+            A[i][j] = i + j + 1;
+            B[i][j] = (i == j) ? 1 : 0;  // matriz identidade
+        }
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 4  // tamanho da matriz (NxN)
+
+void multiplicarMatrizes(int A[N][N], int B[N][N], int C[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+void imprimirMatriz(int M[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", M[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int A[N][N], B[N][N], C[N][N];
+
+    // Inicialização das matrizes
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) {
+            A[i][j] = i + j + 1;
+            B[i][j] = (i == j) ? 1 : 0;  // matriz identidade
+        }
+    // Medir tempo de execução
+    clock_t start = clock();
+
+    multiplicarMatrizes(A, B, C);
+
+    clock_t end = clock();
+    double tempo_execucao = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+    printf("Resultado da multiplicação (sequencial):\n");
+    imprimirMatriz(C);
+
+    printf("Tempo de execução (sequencial): %.6f segundos\n", tempo_execucao);
 
     return 0;
 }
+
