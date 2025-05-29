1f9a60a3c02b2c3992b65a01b5b3e5b3415a03fc
Elocinn <100329391+Elocinn@users.noreply.github.com>
2025-05-28 21:00:43 -0300

    Update paralelo.c

--- paralelo.c
+++ paralelo.c
@@ -1,3 +1,4 @@
+                          
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -29,40 +30,8 @@ int main(int argc, char *argv[]) {
         return 0;
     }
 
-    // Inicialização das matrizes no processo 0
-#include <mpi.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#define N 4  // tamanho da matriz (NxN)
-
-void imprimirMatriz(int mat[N][N]) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%d ", mat[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-int main(int argc, char *argv[]) {
-    int rank, size;
-    int A[N][N], B[N][N], C[N][N];
-    int local_A[N];
-
-    MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    if (size != N) {
-        if (rank == 0)
-            printf("Este programa requer exatamente %d processos.\n", N);
-        MPI_Finalize();
-        return 0;
-    }
-
-    // Inicialização das matrizes no processo 0
-   if (rank == 0) {
+   // Inicialização das matrizes no processo 0
+    if (rank == 0) {
         for (int i = 0; i < N; i++)
             for (int j = 0; j < N; j++) {
                 A[i][j] = i + j + 1;
@@ -93,7 +62,6 @@ int main(int argc, char *argv[]) {
 
     // Gather das linhas calculadas
     MPI_Gather(local_C, N, MPI_INT, C, N, MPI_INT, 0, MPI_COMM_WORLD);
-
     // Obter o tempo máximo entre os processos
     double max_duration;
     MPI_Reduce(&local_duration, &max_duration, 1, MPI_DOUBLE, MPI_MAX, 0, MPI>
@@ -108,4 +76,6 @@ int main(int argc, char *argv[]) {
     MPI_Finalize();
     return 0;
 }
+
+
 
